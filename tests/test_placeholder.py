fb597754b0ccec3867ef4e10ed6d2c2fbba0de8a
Moodyreplicant <erkki70@gmail.com>
2026-06-12 16:36:29 +0300

    chore: add pytest config and test scaffold

---
+++ tests/test_placeholder.py
@@ -0,0 +1,6 @@
+"""Placeholder test to verify pytest discovers and runs tests."""
+
+
+def test_placeholder() -> None:
+    """Trivial test; will be replaced by real tests in later phases."""
+    assert True
